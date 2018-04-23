4cd2ff1d651aac7068e261f781ca59dfe131b4b9
hrsi88 <hrsi88@gmail.com>
2018-04-23 19:28:27 +0300

    Shortend imports

--- Trader/src/TraderClient.java
+++ Trader/src/TraderClient.java
@@ -1,38 +1,31 @@
-import com.binance.api.client.BinanceApiClientFactory;
-import com.binance.api.client.BinanceApiRestClient;
-import com.binance.api.client.domain.OrderSide;
-import com.binance.api.client.domain.OrderType;
-import com.binance.api.client.domain.TimeInForce;
-import com.binance.api.client.domain.account.AssetBalance;
-import com.binance.api.client.domain.account.NewOrder;
-import com.binance.api.client.domain.account.NewOrderResponse;
-import com.binance.api.client.domain.account.Order;
-import com.binance.api.client.domain.account.request.CancelOrderRequest;
-import com.binance.api.client.domain.account.request.OrderRequest;
-import com.binance.api.client.domain.account.request.OrderStatusRequest;
+import com.binance.api.client.*; // BinanceApiClientFactory, BinanceApiRestClient
+import com.binance.api.client.domain.*; // OrderSide, OrderType, TimeInForce
+import com.binance.api.client.domain.account.*; // AssetBalance, NewOrder, NewOrderResponse, Order
+import com.binance.api.client.domain.account.request.*; // CancelOrderRequest, OrderRequest, OrderStatusRequest
 import com.binance.api.client.domain.market.OrderBook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-public class TraderClient {
+public class TraderClient 
+{
 	private static Logger logger = LoggerFactory.getLogger(TraderClient.class);
-
-	private BinanceApiRestClient client; \\used to communicate with binance-api
+	private BinanceApiRestClient client; // used to communicate with binance-api
 	private String baseCurrency;
 	private String tradeCurrency;
 	private String symbol;
 
-	TraderClient(String bCurrency, String tCurrency, String key, String secret) {
-		this.baseCurrency=bCurrency;
-		this.tradeCurrency=tCurrency;
-		BinanceApiClientFactory factory=BinanceApiClientFactory.newInstance(key,secret);
+	TraderClient(String bCurrency, String tCurrency, String key, String secret)
+	{
+		this.baseCurrency = bCurrency;
+		this.tradeCurrency = tCurrency;
+		BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance(key, secret);
 		client = factory.newRestClient();
-		symbol=tCurrency+bCurrency;
+		symbol = tCurrency+bCurrency;
 	}
 
-	//now we need to create the public methods that we need using the api (client)
+	// now we need to create the public methods that we need using the api (client)
 
 	
 }
